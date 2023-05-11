bd672ba6184c205fde3f20bc18a00a02d8b764b6
Lucas Marsiske <47642996+LMarsiske@users.noreply.github.com>
2023-05-11 16:44:46 -0400

    Hopefully this ACTUALLY fixes the character error

--- app/components/logo.tsx
+++ app/components/logo.tsx
@@ -7,7 +7,7 @@ const Logo = () => {
       <a className="my-2 flex items-center space-x-1 text-indigo-500">
         <BoltIcon className="h-8 w-8 flex-shrink-0 mr-3" />
         <span className="font-bold text-3xl font-sans tracking-tight whitespace-nowrap">
-          What&#39;'s Next?
+          What&apos;s Next?
         </span>
       </a>
     </Link>
