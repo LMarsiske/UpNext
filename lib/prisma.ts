fdd12a4689a58cc0c55f74ab614ab3dff0835d34
Lucas Marsiske <47642996+LMarsiske@users.noreply.github.com>
2023-05-28 10:08:04 -0400

    Working on Docker, having trouble with Prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,14 +2,32 @@ import { PrismaClient } from "@prisma/client";
 
 let prisma: PrismaClient;
 
+let dbUrl = process.env.DOCKERIZED
+  ? process.env.DEV_DATABASE_URL
+  : process.env.POSTGRES_PRISMA_URL;
+console.log("process.env.DOCKERIZED", process.env.DOCKERIZED);
+console.log(dbUrl);
+
 if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
+  prisma = new PrismaClient({
+    datasources: {
+      db: {
+        url: dbUrl,
+      },
+    },
+  });
 } else {
   let globalWithPrisma = global as typeof globalThis & {
     prisma: PrismaClient;
   };
   if (!globalWithPrisma.prisma) {
-    globalWithPrisma.prisma = new PrismaClient();
+    globalWithPrisma.prisma = new PrismaClient({
+      datasources: {
+        db: {
+          url: dbUrl,
+        },
+      },
+    });
   }
   prisma = globalWithPrisma.prisma;
 }
