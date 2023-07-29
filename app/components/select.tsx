48ef21236bcd3989f906f2e8e9cf372be3792e56
Lucas Marsiske <47642996+LMarsiske@users.noreply.github.com>
2023-07-29 16:48:04 -0400

    Fixed build errors

--- app/components/select.tsx
+++ app/components/select.tsx
@@ -8,67 +8,67 @@ import CheckIcon from "@mui/icons-material/Check";
 import styles from "@/styles/neon.module.css";
 import { useBackdropStoreSelectors } from "@/stores/backdrop";
 
-export const Select = React.forwardRef(
-  ({ children, ...props }, forwardedRef) => {
-    const [isOpen, setIsOpen] = useState(false);
-    const openBackdrop = useBackdropStoreSelectors.use.openBackdrop();
-    const closeBackdrop = useBackdropStoreSelectors.use.closeBackdrop();
+const Select = React.forwardRef(({ children, ...props }, forwardedRef) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const openBackdrop = useBackdropStoreSelectors.use.openBackdrop();
+  const closeBackdrop = useBackdropStoreSelectors.use.closeBackdrop();
 
-    return (
-      <SelectPrimitive.Root
-        {...props}
-        onOpenChange={(open) => {
-          if (open) {
-            openBackdrop();
-          } else {
-            closeBackdrop();
-          }
-          setIsOpen(open);
-        }}
-      >
-        <SelectPrimitive.Trigger
-          ref={forwardedRef}
-          className="w-2/3 bg-fog dark:bg-davy text-gunmetal dark:text-snow flex justify-between items-center text-lg rounded-xl p-2 font-semibold"
-        >
-          <SelectPrimitive.Value />
-          <SelectPrimitive.Icon asChild>
-            {isOpen ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-          </SelectPrimitive.Icon>
-        </SelectPrimitive.Trigger>
-        <SelectPrimitive.Portal>
-          <SelectPrimitive.Content
-            position="popper"
-            className="w-select max-h-select-height  bg-fog dark:bg-davy text-gunmetal dark:text-snow flex justify-between items-center text-lg rounded-xl mt-1 z-[51] shadow-neon"
-          >
-            <SelectPrimitive.ScrollUpButton>
-              <KeyboardArrowUpIcon />
-            </SelectPrimitive.ScrollUpButton>
-            <SelectPrimitive.Viewport>{children}</SelectPrimitive.Viewport>
-            <SelectPrimitive.ScrollDownButton>
-              <KeyboardArrowDownIcon />
-            </SelectPrimitive.ScrollDownButton>
-          </SelectPrimitive.Content>
-        </SelectPrimitive.Portal>
-      </SelectPrimitive.Root>
-    );
-  }
-);
-
-export const SelectItem = React.forwardRef(
-  ({ children, ...props }, forwardedRef) => {
-    return (
-      <SelectPrimitive.Item
-        {...props}
+  return (
+    <SelectPrimitive.Root
+      {...props}
+      onOpenChange={(open) => {
+        if (open) {
+          openBackdrop();
+        } else {
+          closeBackdrop();
+        }
+        setIsOpen(open);
+      }}
+    >
+      <SelectPrimitive.Trigger
         ref={forwardedRef}
-        className={`flex justify-between items-center w-select-item p-2 my-1 data-[highlighted]:bg-davy data-[highlighted]:text-snow dark:data-[highlighted]:bg-fog dark:data-[highlighted]:text-gunmetal z-[51]`}
+        className="w-2/3 bg-fog dark:bg-davy text-gunmetal dark:text-snow flex justify-between items-center text-lg rounded-xl p-2 font-semibold"
       >
-        <SelectPrimitive.ItemText className="cursor-pointer">
-          {children}
-        </SelectPrimitive.ItemText>
-        <SelectPrimitive.ItemIndicator>
-          <CheckIcon />
-        </SelectPrimitive.ItemIndicator>
-      </SelectPrimitive.Item>
-    );
-  }
-);
+        <SelectPrimitive.Value />
+        <SelectPrimitive.Icon asChild>
+          {isOpen ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+        </SelectPrimitive.Icon>
+      </SelectPrimitive.Trigger>
+      <SelectPrimitive.Portal>
+        <SelectPrimitive.Content
+          position="popper"
+          className="w-select max-h-select-height  bg-fog dark:bg-davy text-gunmetal dark:text-snow flex justify-between items-center text-lg rounded-xl mt-1 z-[51] shadow-neon"
+        >
+          <SelectPrimitive.ScrollUpButton>
+            <KeyboardArrowUpIcon />
+          </SelectPrimitive.ScrollUpButton>
+          <SelectPrimitive.Viewport>{children}</SelectPrimitive.Viewport>
+          <SelectPrimitive.ScrollDownButton>
+            <KeyboardArrowDownIcon />
+          </SelectPrimitive.ScrollDownButton>
+        </SelectPrimitive.Content>
+      </SelectPrimitive.Portal>
+    </SelectPrimitive.Root>
+  );
+});
+Select.displayName = "Select";
+
+const SelectItem = React.forwardRef(({ children, ...props }, forwardedRef) => {
+  return (
+    <SelectPrimitive.Item
+      {...props}
+      ref={forwardedRef}
+      className={`flex justify-between items-center w-select-item p-2 my-1 data-[highlighted]:bg-davy data-[highlighted]:text-snow dark:data-[highlighted]:bg-fog dark:data-[highlighted]:text-gunmetal z-[51]`}
+    >
+      <SelectPrimitive.ItemText className="cursor-pointer">
+        {children}
+      </SelectPrimitive.ItemText>
+      <SelectPrimitive.ItemIndicator>
+        <CheckIcon />
+      </SelectPrimitive.ItemIndicator>
+    </SelectPrimitive.Item>
+  );
+});
+SelectItem.displayName = "SelectItem";
+
+export { Select, SelectItem };
